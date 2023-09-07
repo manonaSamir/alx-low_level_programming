baf81f9818f97c18671962dae3bb56eaecf47332
manonaSamir <bena.yalla@yahoo.com>
2023-09-07 22:17:12 +0300

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: 'programme prints character'
+ * Description: 'C program that prints the size of various types on the computer'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
