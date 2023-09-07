809cd6f503f3147eec53e95894dac278c23c665f
manonaSamir <bena.yalla@yahoo.com>
2023-09-07 23:09:31 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char print = 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n';
+putchar(print);
+return (1);
+}
+
