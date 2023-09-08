9316c4f9e8dbf7d20776ae0afd488c2552b3636c
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 16:21:14 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - variables_if_else_while
+ *
+ * Description: 'print_alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
