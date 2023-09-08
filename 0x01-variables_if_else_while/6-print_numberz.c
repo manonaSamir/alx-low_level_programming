313fa7debfbbdba2dbaf61b4a6ddb6b3f434eaf7
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 16:18:24 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - variables_if_else_while
+ *
+ * Description: '6-print_numberz.c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
