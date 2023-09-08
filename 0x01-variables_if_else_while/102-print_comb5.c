649828d853a88774674d829ec7383fe0e3e54f59
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 23:15:26 +0300

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,18 +13,24 @@ int main(void)
 int a, b, c, d;
 for (a = 0; a < 10; a++)
 {
-for (b = 0; b < 9; b++)
+b = 0;
+if(a > 0)
+{
+    b = a +1;
+}
+for (; b < 9; b++)
 {
 for (c = 0; c < 10; c++)
 {
-for (d = 1; d < 10; d++)
+d = c + 1;
+for (; d < 10; d++)
 {
 putchar('0' + a);
 putchar('0' + b);
 putchar(' ');
 putchar('0' + c);
 putchar('0' + d);
-if (!(d == 9 && b == 8))
+if (!(c == 8 && d == 9))
 {
 putchar(',');
 putchar(' ');
