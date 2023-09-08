b5ed7e6ee6c04ad9c83d6641858af62011eda974
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 15:48:56 +0300

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - variables_if_else_while
+ *
+ * Description: 'print_alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
