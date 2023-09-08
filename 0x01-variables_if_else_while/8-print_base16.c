0505d8f16b1a19a54349a802bdd23e0b45806c82
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 17:53:51 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - variables_if_else_while
+ *
+ * Description: '8-print_base16.c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 0; i < 16; i++)
+{
+//printf("%X",i);
+if(i < 10){
+    putchar('0' + i);
+}
+else{
+ putchar(87 + i);
+}
+
+
+}
+
+putchar('\n');
+return (0);
+}
