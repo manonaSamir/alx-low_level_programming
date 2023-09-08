3906e5f27cb17d608089095a7a787f71893e48fe
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 15:38:19 +0300

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("%d is greater than 5\n", n);
+printf("Last digit of %d is greater than 5\n", n);
 }
 else if (n == 0)
 {
-printf("%d is 0\n", n);
+printf("Last digit of %d is 0\n", n);
 }
 else if (n < 6 && n != 0)
 {
-printf("%d is less than 6 and not 0\n", n);
+printf("Last digit of %d is less than 6 and not 0\n", n);
 }
 
 return (0);
