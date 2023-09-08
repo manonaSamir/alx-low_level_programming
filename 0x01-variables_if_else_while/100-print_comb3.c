b545ef191d0a06f5df1951c444aea5922e5dfb5c
manonaSamir <bena.yalla@yahoo.com>
2023-09-08 20:06:08 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - variables_if_else_while
+ *
+ * Description: '9-print_comb.c'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i,j;
+for (i = 0 ; i < 9 ; i++)
+{
+j = i + 1;
+for(j; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i < 8)
+{
+putchar(',');
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
