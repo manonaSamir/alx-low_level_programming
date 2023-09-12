a84b44200e2c43441c39422acae4f257946a948c
manonaSamir <bena.yalla@yahoo.com>
2023-09-12 22:21:01 +0300

    print_last_digit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
  * @n: print from this number
+ * Return : 
  */
 
 int print_to_98(int n)
@@ -11,16 +11,16 @@ int print_to_98(int n)
 int i;
 if (n == 98)
 {
-_putchar('0' + 98);
+printf("98");
 }
 else if (n < 98)
 {
 for (i = n ; i <= 98 ; i++)
 {
-_putchar('0' + i);
+printf("%d", i);
 if (i < 98)
 {
-_putchar(", ");
+printf(", ");
 }
 }
 }
@@ -28,10 +28,10 @@ else if (n > 98)
 {
 for (i = n ; i >= 98 ; i--)
 {
-_putchar('0' + i);
+printf("%d", i);
 if (i > 98)
-_putchar(", ");
+printf(", ");
 }
 }
-_putchar("\n");
+printf("\n");
 }
