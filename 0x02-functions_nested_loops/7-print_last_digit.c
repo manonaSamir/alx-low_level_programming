9189c5a86bd22fcfeeb90285498bed9b83c3d7ba
manonaSamir <bena.yalla@yahoo.com>
2023-09-12 19:47:37 +0300

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Write a function that prints the last digit of a number.
+ * @n: value use to find the absolute value.
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}
