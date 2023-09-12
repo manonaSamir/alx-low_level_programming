dd12322d7d9916e7a5f55b4604f8dd4adf76c5c3
manonaSamir <bena.yalla@yahoo.com>
2023-09-12 17:55:00 +0300

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+/**
+ * main - variables_if_else_while
+ *
+ * Description: '1-alphabet.c'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
