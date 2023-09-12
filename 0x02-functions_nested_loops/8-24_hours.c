1d56986c38ff8bbf1253d835bb02c33de82340ab
manonaSamir <bena.yalla@yahoo.com>
2023-09-12 20:19:24 +0300

    print_last_digit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - function that prints every minute of the day, starting from 00:00 to 23:59.
+ * jack_bauer - print every minute of the day, starting from 00:00 - 23:59.
  * Return: void
  */
 
