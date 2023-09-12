bafe692a53a3150e95c618a1edcc65f8ddce87c7
manonaSamir <bena.yalla@yahoo.com>
2023-09-13 00:08:44 +0300

    print_last_digit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+long int i, last = 0;
+for (i = 1; i <= 50; i++)
+{
+if ( i + last < 50)
+{
+printf("%ld", i + last);
+if ((i + last) != 49)
+{
+printf(", ");
+}
+last = i;
+}
+}
+return (0);
+}
