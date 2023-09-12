d137d663ba61b6e4e1446a8d1e31091e264a491e
manonaSamir <bena.yalla@yahoo.com>
2023-09-12 23:44:05 +0300

    print_last_digit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,19 +7,15 @@
  */
 int main(void)
 {
-unsigned long int sum, i;
-for (i = 0; i < 1024; i++)
+unsigned long int sum = 0, i;
+for (i = 0; i < 1024; ++i)
 {
-if ((i % 3) == 0)
-{
-sum += i;
-}
-else if ((i % 5) == 0)
+if (((i % 3) == 0) || ((i % 5) == 0))
 {
 sum += i;
 }
 }
-printf("%lu\n", sum);
+printf("%d\n", sum);
 printf("\n");
 return (0);
 }
