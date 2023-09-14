1b3de5faf200121bc46dbdd9ecbb3e874e78ecc1
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 20:47:46 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-long int n = 612852475143;
+long long int n = 612852475143;
 long result = 0;
 int i;
 while (n % 2 == 0)
@@ -21,7 +21,6 @@ for (i = 3; i <= sqrt(n); i = i + 2)
 {
 while (n % i == 0)
 {
-printf("%d", i);
 n = n / i;
 }
 }
