2402da2c1782da49c259cfb630383a34a7c4bd1c
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 17:19:26 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-    int i;
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-           printf("%d", i);
-        }
-        putchar(' ');
-    }
-    return (0);
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+putchar(' ');
+}
+return (0);
 }
