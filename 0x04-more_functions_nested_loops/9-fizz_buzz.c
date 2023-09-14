3cd48084ba8b8c9ab9b5c2a301747935665b5b5d
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 17:18:20 +0300

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+/**
+ * main -  program that prints the numbers from 1 to 100.
+ *  Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+           printf("%d", i);
+        }
+        putchar(' ');
+    }
+    return (0);
+}
