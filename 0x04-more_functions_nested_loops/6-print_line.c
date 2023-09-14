398aa8b1a5bf086b1c6e9c5cf2d4c72538307a81
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 15:27:14 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,7 +5,7 @@
  * @n: the number of times the character _ should be printed
  * Return: 0
  */
-int print_line(int n)
+void print_line(int n)
 {
 int i;
 for (i = 0; i <= n; i++)
@@ -13,5 +13,4 @@ for (i = 0; i <= n; i++)
 _putchar('0' + (i * n));
 _putchar('\n');
 }
-return (0);
 }
