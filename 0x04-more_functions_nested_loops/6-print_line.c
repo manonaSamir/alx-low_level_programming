8a180978b8b175678e8e034279731f914f628fac
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 12:10:03 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,14 @@
 /**
  * print_line - function that draws a straight line in the terminal.
  * @n: he number of times the character _ should be printed
- * Return: 0 
+ * Return: 0
  */
 int print_line(int n)
 {
 int i;
 for (i = 0; i <= n; i++)
 {
-_putchar('0' + (i*n));
+_putchar('0' + (i * n));
 _putchar('\n');
 }
 }
