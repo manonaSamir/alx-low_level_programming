bf1cb59fe928f0bfb6fafb4e2c29c31aeb6d375a
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 21:53:44 +0300

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer.
+ * @n: the character to be checked
+ * Return: 0
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+n = -n;
+_putchar('-');
+}
+if (n >= 10 && n < 100)
+{
+_putchar('0' + n / 10);
+}
+else if (n >= 100 && n < 1000)
+{
+_putchar((n / 100) + '0');
+_putchar(((n / 10) % 10) + '0');
+}
+else
+{
+if (n / 10)
+{
+print_number(n / 10);
+}
+}
+putchar((n % 10) + '0');
+}
