1814921010950ec4a379e3507424b75d5f393b7b
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 18:02:30 +0300

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a triangle.
+ * @size: the number of times the character _ should be printed
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+size = 10;
+int i, j, k;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = i+1; j <= size; j++)
+{
+_putchar(" ");
+}
+for (k = 0; k <= i; k++)
+{
+_putchar("#");
+}
+_putchar('\n');
+}
+}
+}
