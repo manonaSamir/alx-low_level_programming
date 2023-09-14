c12e23b8c28980993935a44fdebac7d267782694
manonaSamir <bena.yalla@yahoo.com>
2023-09-14 22:30:21 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,16 +11,9 @@ if (n < 0)
 n = -n;
 _putchar('-');
 }
-if (n > 10)
-{
-_putchar('0' + n / 10);
-}
-else
-{
-if (n / 10)
+if ((n / 10) > 0)
 {
 print_number(n / 10);
-}
 }
 _putchar((n % 10) + '0');
 }
