1ea48d2aa2a349befdb849f4e00a23ca511b4ecd
manonaSamir <bena.yalla@yahoo.com>
2023-09-18 16:57:26 +0300

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "4-print_rev.c"
+#include "3-puts.c"
 
 /**
  * rev_string - reverses a string.
@@ -8,5 +9,6 @@
 
 void rev_string(char *s)
 {
+_puts(s);
 print_rev(s);
 }
