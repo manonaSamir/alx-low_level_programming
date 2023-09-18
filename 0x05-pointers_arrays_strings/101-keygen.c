9b26a142390812e9d09f4769381f4e8b108baa00
manonaSamir <bena.yalla@yahoo.com>
2023-09-18 23:57:42 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Program that generates random valid passwords
+ * Return: 0 if works
+ */
+
+int main(void)
+{
+char array[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+char password[100];
+srand(time(NULL));
+sum = 0;
+i = 0;
+for (i =0; sum < (2772 - 122); i++)
+{
+r = rand() % 62;
+password[i] = array[r];
+sum = sum + password[i];
+i++;
+}
+r = 2772 - sum;
+password[i] = r;
+printf("%c", password);
+return (0);
+}
