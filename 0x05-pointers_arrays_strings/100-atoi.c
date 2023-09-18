a5ff217ea54951d949a3eb7d3a000a38331d76cc
manonaSamir <bena.yalla@yahoo.com>
2023-09-18 23:05:50 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to copy to the destination.
+ * Return: a copy of the src.
+ */
+
+int _atoi(char *s)
+{
+int i, minus, boolean;
+long result;
+result = boolean = 0;
+minus = -1;
+for (i = 0; i < strlen(s); i++)
+{
+if (s[i] == '-')
+minus *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+result *= 10;
+result -= (s[i] - '0');
+boolean = 1;
+}
+else if (boolean == 1)
+{
+break;
+}
+}
+result *= minus;
+return (result);
+}
