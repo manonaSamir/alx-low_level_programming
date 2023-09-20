1f7d9bf4e4ff632c8ae990725e7c15f8b455c1d5
manonaSamir <bena.yalla@yahoo.com>
2023-09-20 22:32:59 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,8 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n && i <= strlen(src); i++)
+int k = strlen(src);
+for (i = 0; i < n && i <= k; i++)
 {
 dest[i] = src[i];
 }
