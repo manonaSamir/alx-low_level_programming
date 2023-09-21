bad2895628ec650026727a4b89457abc7fdb37b7
manonaSamir <bena.yalla@yahoo.com>
2023-09-21 16:16:16 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *m)
 char sep[] = { ' ', '\t', '\n', ',', ';', '!',
 '.', '?', '\"', '(', ')', '{', '}'};
 int diff = 'a' - 'A', i = 0, j, len = strlen(m), sepLen = strlen(sep);
-for (; i <= len; i++)
+for (; i < len; i++)
 {
 if (!(m[i] >= 'a' && m[i] <= 'z') && m[i] != '\0')
 {
