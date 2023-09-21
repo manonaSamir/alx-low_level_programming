d8a7b04272eb0e62dff87fcce0ca843053b8a609
manonaSamir <bena.yalla@yahoo.com>
2023-09-21 16:25:19 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,35 @@
 #include <string.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
 * cap_string - This function capitalizes all words of a string.
-* @s: sring to be processed.
+* @m: sring to be processed.
 * Return: pointer to the modified string.
 */
 
 
-char *cap_string(char *s)
+char *cap_string(char *m)
 {
 char sep[] = { ' ', '\t', '\n', ',', ';', '!',
 '.', '?', '\"', '(', ')', '{', '}'};
-int diff = 'a' - 'A', i = 0, j, len = strlen(s), sepLen = strlen(sep);
+int diff = 'a' - 'A', i = 0, j, len = strlen(m), sepLen = strlen(sep);
 for (; i < len; i++)
 {
-if (!(s[i] >= 'a' && s[i] <= 'z') && s[i] != '\0')
+if (!(m[i] >= 'a' && m[i] <= 'z') && m[i] != '\0')
 {
 for (j = 0; j <= sepLen; j++)
 {
-if (sep[j] == s[i])
+if (sep[j] == m[i])
 {
-if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+if (m[i + 1] >= 'a' && m[i + 1] <= 'z')
 {
-s[i + 1] = s[i + 1] - diff;
+m[i + 1] = m[i + 1] - diff;
 continue;
 }
 }
 }
 }
 }
-return (s);
+return (m);
 }
