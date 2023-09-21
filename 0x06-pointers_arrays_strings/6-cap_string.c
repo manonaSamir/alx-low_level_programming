8b0d6b9c8540ac2fb9339e390a6f642eda5824ce
manonaSamir <bena.yalla@yahoo.com>
2023-09-21 14:29:15 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,26 +16,24 @@ char sep[] = { ' ', '\t', '\n', ',', ';', '!',
 int diff = 'a' - 'A', i = 0, j, len = strlen(m), sepLen = strlen(sep);
 for (; i <= len; i++)
 {
+if (m[i + 1] >= 'a' && m[i + 1] <= 'z')
+{
 if (!(isalpha(m[i]) || isdigit(m[i])))
 {
 for (j = 0; j <= sepLen; j++)
 {
 if (sep[j] == m[i])
 {
-if (m[i + 1] >= 'a' && m[i + 1] <= 'z')
-{
 m[i + 1] = m[i + 1] - diff;
 break;
 }
+}
+}
+}
 else
 {
-i++;
-continue;
-}
-}
-}
+break;
 }
 }
 return (m);
 }
-
