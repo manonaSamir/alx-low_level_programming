726a084228b80f30057281c3e073ad0d8ab8b763
manonaSamir <bena.yalla@yahoo.com>
2023-09-21 14:16:08 +0300

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @m: array to convert upper
+ * Return: the resulting string
+ */
+
+char *cap_string(char *m)
+{
+char sep[] = { ' ', '\t', '\n', ',', ';', '!', '.', '?', '\"', '(', ')', '{', '}'};
+int diff = 'a' - 'A', i = 0,j , len = strlen(m), sepLen = strlen(sep);
+for (; i <= len; i++)
+{
+if (m[i + 1] >= 'a' && m[i + 1] <= 'z')
+{
+if (!(isalpha(m[i]) || isdigit(m[i])))
+{
+for (j = 0; j <= sepLen; j++)
+{
+if (sep[j] == m[i])
+{
+m[i + 1] = m[i + 1] - diff;
+break;
+}
+}
+}
+}
+}
+return (m);
+}
