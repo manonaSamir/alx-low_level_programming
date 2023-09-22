4c3790f0977c1004a243623f0b93c0a90d8bc32d
manonaSamir <bena.yalla@yahoo.com>
2023-09-22 14:20:19 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,12 +16,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int carry = 0, index = 0, index2;
 char *s1 = n1, *s2 = n2;
+
 while (*s1 != 0)
-{
-s1++;
-}
+	s1++;
 while (*s2 != 0)
-{s2++;}
+	s2++;
 size_r--;
 r[size_r] = 0;
 s1--;
@@ -39,7 +38,7 @@ index++;
 s2--;
 s1--;
 if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
-{return (0);}
+	return (0);
 }
 while (s1 != n1 - 1)
 {
@@ -53,9 +52,7 @@ r[index] -= 10;
 s1--;
 index++;
 if (size_r == index && (s1 != n1 - 1 || carry == 1))
-{
-return (0);
-}
+	return (0);
 }
 while (s2 != n2 - 1)
 {
@@ -69,7 +66,7 @@ r[index] -= 10;
 s2--;
 index++;
 if (size_r == index && (s2 != n2 - 1 || carry == 1))
-{return (0);}
+	return (0);
 }
 if (carry == 1)
 {
@@ -77,9 +74,7 @@ r[index] = '1';
 r[index + 1] = 0;
 }
 else
-{
-r[index--] = 0;
-}
+	r[index--] = 0;
 index2 = 0;
 while (index2 <= index)
 {
