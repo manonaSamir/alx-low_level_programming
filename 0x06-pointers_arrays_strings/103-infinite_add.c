ea15b4b264464022015d32752408e67275d972ce
manonaSamir <bena.yalla@yahoo.com>
2023-09-22 21:56:58 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,4 @@
 #include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * infinite_add - adds two integers stored as strings
@@ -15,10 +13,26 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int sum = atoi(n1) + atoi(n2);
-int length = snprintf(NULL, 0, "%d", sum);
-if (length >= size_r)
-return (0);
-snprintf(r, size_r, "%d", sum);
+int len1 = strlen(n1);
+int len2 = strlen(n2);
+int lenR = len1 > len2 ? len1 : len2;
+if (lenR + 1 > size_r)
+    return (0);
+int carry = 0;
+for (int i = 0; i < lenR; i++)
+{
+int digit1 = i < len1 ? n1[len1 - 1 - i] - '0' : 0;
+int digit2 = i < len2 ? n2[len2 - 1 - i] - '0' : 0;
+int sum = digit1 + digit2 + carry;
+r[lenR - i] = sum % 10 + '0';
+carry = sum / 10;
+}
+if (carry > 0)
+{
+if (lenR + 2 > size_r)
+    return (0);
+memmove(r + 1, r, lenR + 1); // shift right by one position
+r[0] = carry + '0'; // store the carry
+}
 return (r);
 }
