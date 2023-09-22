4a0ca72a69a7934968c8462bc4e65991029b31f7
manonaSamir <bena.yalla@yahoo.com>
2023-09-22 15:16:12 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,15 +14,17 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int carry = 0, index = 0, index2;
-char *s1, *s2;
+char *s1 = n1, *s2 = n2;
 
-for (*s1 = n1; *s1 != 0; s1++)
-for (*s2 = n2; *s2 != 0; s2++)
+for (; *s1 != 0; s1++)
+for (; *s2 != 0; s2++)
 	s1--;
 	s2--;
 	size_r--;
 r[size_r] = 0;
-for (; s2 != n2 - 1 && s1 != n1 - 1; index++; s2--; s1--)
+for (; s2 != n2 - 1 && s1 != n1 - 1; index++)
+	s2--;
+	s1--;
 	r[index] = *s2 - '0' + *s1 + carry;
 	carry = 0;
 	if (r[index] > '9')
@@ -30,7 +32,8 @@ for (; s2 != n2 - 1 && s1 != n1 - 1; index++; s2--; s1--)
 		r[index] -= 10;
 	if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
 		return (0);
-for (; s1 != n1 - 1; index++; s1--)
+for (; s1 != n1 - 1; index++)
+	s1--;
 	r[index] = *s1 + carry;
 	carry = 0;
 	if (r[index] > '9')
@@ -38,7 +41,8 @@ for (; s1 != n1 - 1; index++; s1--)
 		r[index] -= 10;
 	if (size_r == index && (s1 != n1 - 1 || carry == 1))
 		return (0);
-for (; s2 != n2 - 1; index++; s2--)
+for (; s2 != n2 - 1; index++)
+	s2--;
 	r[index] = *s2 + carry;
 	carry = 0;
 	if (r[index] > '9')
@@ -51,7 +55,8 @@ if (carry == 1)
 	r[index + 1] = 0;
 else
 	r[index--] = 0;
-for (index2 = 0; index2 <= index; index--; index2++)
+for (index2 = 0; index2 <= index; index--)
+	index2++;
 	carry = r[index];
 	r[index] = r[index2];
 	r[index2] = carry;
