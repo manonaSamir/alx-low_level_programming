7c7276987a5195e05d7c5749d9377bce75ecb8ae
manonaSamir <bena.yalla@yahoo.com>
2023-09-23 19:08:08 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -1,59 +1,24 @@
-#include "main.h"
+
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* rot13 - function yo rotate a string
+* @str: string we want to rotate
+* Return: pointer to the converted string */
+char *rot13 (char *str)
 {
-        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-        char r[100];
-        char r2[10];
-        char r3[11];
-        char *res;
-
-        res = infinite_add(n, m, r, 100);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "1234567890";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        return (0);
+int i = 0, j;
+while (str[i])
+{
+j = 0;
+while (j < 13 && ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
+{
+if ((str[i] >= 'a' && str[i] <= 'm')|| (str[i] >= 'A' && str[i] <= 'M'))
+    str[i] += 13;
+else
+    str[i]-=13;
+j++;
+}
+i++;
+}
+return (str);
 }
