71744d4700c5cf68feb9857a542e04c5920cdae7
manonaSamir <bena.yalla@yahoo.com>
2023-09-23 19:25:05 +0300

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,21 +1,24 @@
-
 #include <stdio.h>
+
 /**
 * rot13 - function yo rotate a string
 * @str: string we want to rotate
-* Return: pointer to the converted string */
-char *rot13 (char *str)
+* Return: pointer to the converted string
+ */
+
+char *rot13(char *str)
 {
 int i = 0, j;
 while (str[i])
 {
 j = 0;
-while (j < 13 && ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
+while (j < 13 && ((str[i] >= 'a' && str[i] <= 'z')
+|| (str[i] >= 'A' && str[i] <= 'Z')))
 {
-if ((str[i] >= 'a' && str[i] <= 'm')|| (str[i] >= 'A' && str[i] <= 'M'))
-    str[i] += 13;
+if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+str[i] += 13;
 else
-    str[i]-=13;
+str[i] -= 13;
 j++;
 }
 i++;
