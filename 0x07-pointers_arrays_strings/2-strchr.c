492556cbb422e79c7a2151cdb9c6cfd35d9ac2d4
manonaSamir <bena.yalla@yahoo.com>
2023-09-25 15:33:13 +0300

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,9 +16,9 @@ char *_strchr(char *s, char c)
 unsigned int i;
 for (i = 0; i < strlen(s); i++)
 {
-if (s[i] == c || c == '\0')
+if (s[i] == c)
 {
-return ((s + i));
+return (s + i);
 }
 }
 return (NULL);
