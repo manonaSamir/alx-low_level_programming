e822f630509cb207f57247d6d7ae987b6d72f24a
manonaSamir <bena.yalla@yahoo.com>
2023-09-25 15:26:54 +0300

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * _strchr - that locates a character in a string.
  * @s: string to search for the character in.
@@ -13,7 +13,7 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
-for (i = 0; s[i] < strlen(s); i++)
+for (i = 0; i < strlen(s); i++)
 {
 if (s[i] == c || c == '\0')
 {
