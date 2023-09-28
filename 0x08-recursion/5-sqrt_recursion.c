cf84e46e6ad6d587529c7e79b6c39566986fa569
manonaSamir <bena.yalla@yahoo.com>
2023-09-28 14:50:39 +0300

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: value to be manipulated.
+ * @y: manipulator
+ *
+ * Return: Sum as Int
+ */
+
+int _sqrt_recursion(int n)
+{
+int static i = 0;
+if (n < 0)
+		return (-1);
+if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+    i++;
+	return (_sqrt_recursion(n));
+}
