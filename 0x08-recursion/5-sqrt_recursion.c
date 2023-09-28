62277b12aabc87778987a2e18c82b9945f276940
manonaSamir <bena.yalla@yahoo.com>
2023-09-28 15:11:02 +0300

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,15 +10,20 @@
  * Return: Sum as Int
  */
 
-int _sqrt_recursion(int n)
+int Set_sqrt_recursion(int n ,int i)
 {
-static int i = 0;
 if (n < 0)
 		return (-1);
 if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-i++;
-	return (_sqrt_recursion(n));
+	return (Set_sqrt_recursion(n, i + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+		return (-1);
+return (Set_sqrt_recursion(n, 0));
 }
