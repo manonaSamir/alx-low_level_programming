b806601e637f374375e0f7764b7347f4fddcc37a
manonaSamir <bena.yalla@yahoo.com>
2023-09-28 15:14:33 +0300

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,11 +12,11 @@
 int Set_sqrt_recursion(int n, int i)
 {
 if (n < 0)
-    return (-1);
+return (-1);
 if (i * i > n)
-    return (-1);
+return (-1);
 if (i * i == n)
-    return (i);
+return (i);
 return (Set_sqrt_recursion(n, i + 1));
 }
 
