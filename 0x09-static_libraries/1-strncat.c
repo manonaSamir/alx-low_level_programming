737e75da89bedc46fbcc726fb74482da2622a036
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 23:42:09 +0300

    static_libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,30 +1,22 @@
+#include "2-strlen.c"
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
+ * *_strncat - concatenates two strings
+ * @dest: pointer to first value
+ * @src: pointer to second value
+ *@n: byte of string
  * Return: a pointer to the resulting string
  */
-char *_strcat(char *dest, char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
+int i, j = 0;
+int k = _strlen(src);
+for (i = _strlen(dest); j < n && j <= k; i++)
+{
+dest[i] = src[j];
+j++;
+}
+return (dest);
 }
