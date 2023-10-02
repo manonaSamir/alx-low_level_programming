37f93857ba41e5799438be59027280c543d68111
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 18:29:33 +0300

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,35 +9,29 @@
  *
  * Return: 0 (Success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	num = atoi(argv[1]);
-	result = 0;
-
-	if (num < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-
-	printf("%d\n", result);
-	return (0);
+int coins[] = {25, 10, 5, 2, 1};
+int num = atoi(argv[1]), i, j = 0, len = sizeof(coins);
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+if (num < 0)
+{
+printf("0\n");
+return (0);
+}
+for (i = 0; i <= len && num > 0; i++)
+{
+while (num >= coins[i])
+{
+j++;
+num -= coins[i];
+}
+}
+printf("%d\n", j);
+return (0);
 }
