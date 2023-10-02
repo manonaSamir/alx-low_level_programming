6031298da707aa82d70f7751cc5e03115928f646
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 18:18:50 +0300

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[])
 {
 int coins[] = {25, 10, 5, 2, 1};
-int num = atoi(argv[1]), i, j = 0, len = sizeof(coins[i]);
+int num = atoi(argv[1]), i, j = 0;
 if (argc != 2)
 {
 printf("Error\n");
