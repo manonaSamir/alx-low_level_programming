b63b2a906f06689908e3513ad1a146340f9e673a
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 16:15:53 +0300

    argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+/**
+ * main - multiplies two numbers
+ * @argc: counts the arguments.
+ * @argv: string array for the arguments entered
+ *
+ * Return: 0 (Sucess) else 1 (Failure).
+ */
+
+int main(int argc, char *argv[])
+{
+    int i, sum = 1;
+    if (argc > 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    for (i = 1; i < argc; i++)
+        sum *= *argv[i];
+    printf("%d\n", sum);
+    return (0);
+}
