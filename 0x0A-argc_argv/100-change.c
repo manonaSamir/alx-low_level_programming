c6e45d7f4fb5650e7b578aa26c0296b0a4d4071b
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 18:32:25 +0300

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,8 +12,8 @@
 
 int main(int argc, char *argv[])
 {
+int num = atoi(argv[1]), i, j = 0;
 int coins[] = {25, 10, 5, 2, 1};
-int num = atoi(argv[1]), i, j = 0, len = sizeof(coins);
 if (argc < 2)
 {
 printf("Error\n");
@@ -24,7 +24,7 @@ if (num < 0)
 printf("0\n");
 return (0);
 }
-for (i = 0; i < len && num >= 0; i++)
+for (i = 0; i < 5 && num >= 0; i++)
 {
 while (num >= coins[i])
 {
