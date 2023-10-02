7f5d1f0bb656f029f527deb4710ffa4956bce2be
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 22:52:37 +0300

    static_libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,7 @@
 #include "main.h"
 
-/**
- * main - test function.
- * Return: (0) if success.
- */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to
-be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
