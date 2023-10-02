b57be899eb9e7ce09712632c29fb20c5e6c10ef1
manonaSamir <bena.yalla@yahoo.com>
2023-10-02 16:16:28 +0300

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,14 @@
 
 int main(int argc, char *argv[])
 {
-    int i, sum = 1;
-    if (argc > 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    for (i = 1; i < argc; i++)
-        sum *= *argv[i];
-    printf("%d\n", sum);
-    return (0);
+int i, sum = 1;
+if (argc > 2)
+{
+printf("Error\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+sum *= *argv[i];
+printf("%d\n", sum);
+return (0);
 }
