4ac8425d5ed6e0689ec101568ca97e7ce56d7147
manonaSamir <bena.yalla@yahoo.com>
2023-10-04 21:27:07 +0300

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,35 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strdup - function that returns a pointer to a newly allocated string.
- * @str: string to allocate memory for.
+ * str_concat - this function concatenates two strings.
+ * @s1: string 1/2 to concat.
+ * @s2: string 2/2 to concat.
  *
- * Return: Pointer to string in memory.
+ * Return: Pointer to concatenated string.
  */
-char *_strdup(char *str)
+
+char *str_concat(char *s1, char *s2)
 {
-int i;
-char *s;
-if (str == 0)
-return (0);
-for (i = 0; str[i]; i++)
-s = malloc(sizeof(char) * (i + 1));
-if (s == 0)
-return (0);
-s[i] = '\0';
-free(s);
-return (s);
+int a, b, c;
+char *str;
+a = b = 0;
+if (s1 != NULL)
+for (a = 0; s1[a]; a++)
+if (s2 != NULL)
+for (b = 0; s2[b]; b++)
+str = malloc(sizeof(char) * (a + b + 1));
+if (str == NULL)
+return (NULL);
+c = 0;
+while (c < (a + b))
+{
+if (c < a)
+str[c] = s1[c];
+else
+str[c] = s2[c - a];
+c++;
+}
+free(str);
+return (str);
 }
