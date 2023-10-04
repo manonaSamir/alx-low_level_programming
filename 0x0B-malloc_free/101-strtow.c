1fddc266279bc2ff34458d6feadaec9e6a8940b9
manonaSamir <bena.yalla@yahoo.com>
2023-10-04 22:29:40 +0300

    malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,20 +10,20 @@
 
 int number(char *str)
 {
-	int a, num = 0;
+    int a, num = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			for (; str[a] != ' ' && str[a] != '\0'; a++)
-				str++;
-			num++;
-		}
-	}
-	return (num);
+    for (a = 0; str[a] != '\0'; a++)
+    {
+        if (*str == ' ')
+            str++;
+        else
+        {
+            for (; str[a] != ' ' && str[a] != '\0'; a++)
+                str++;
+            num++;
+        }
+    }
+    return (num);
 }
 
 /**
@@ -34,9 +34,9 @@ int number(char *str)
 
 void free_everything(char **string, int i)
 {
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
+    for (; i > 0;)
+        free(string[--i]);
+    free(string);
 }
 
 /**
@@ -47,44 +47,46 @@ void free_everything(char **string, int i)
 
 char **strtow(char *str)
 {
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
-
-	if (str == 0 || *str == 0)
-		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
-			{
-				free_everything(words, b);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-			b++; c = 0; length = 0; str++;
-		}
-	}
-	return (words);
+int total_words = 0, b = 0, c = 0, length = 0;
+char **words, *found_word;
+if (str == 0 || *str == 0)
+return (NULL);
+total_words = number(str);
+if (total_words == 0)
+return (NULL);
+words = malloc((total_words + 1) * sizeof(char *));
+if (words == 0)
+return (NULL);
+for (; *str != '\0' && b < total_words;)
+{
+if (*str == ' ')
+str++;
+else
+{
+found_word = str;
+for (; *str != ' ' && *str != '\0';)
+{
+length++;
+str++;
+}
+words[b] = malloc((length + 1) * sizeof(char));
+if (words[b] == 0)
+{
+free_everything(words, b);
+return (NULL);
+}
+while (*found_word != ' ' && *found_word != '\0')
+{
+words[b][c] = *found_word;
+found_word++;
+c++;
+}
+words[b][c] = '\0';
+b++;
+c = 0;
+length = 0;
+str++;
+}
+}
+return (words);
 }
