bcc98112c28f0003d495c28af2e3cac92bd57010
manonaSamir <bena.yalla@yahoo.com>
2023-10-04 22:04:32 +0300

    malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,27 +9,35 @@
 
 char **strtow(char *str)
 {
-char **d;
-int i;
-int j = 0;
-int con = 0;
-if (str == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] != 32)
-con++;
-}
-d = malloc(sizeof(char) * con);
-if (d == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] != 32)
-{
-*d[j] = str[i];
-j++;
-}
-}
-return (d);
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+			con++;
+	}
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+		{
+			*d[j] = str[i];
+			j++;
+		}
+		else
+		{
+		}
+	}
+	return (d);
 }
