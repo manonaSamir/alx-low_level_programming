7b2802d577d057838cc913a26bae690246937241
manonaSamir <bena.yalla@yahoo.com>
2023-10-05 22:37:59 +0300

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,31 +11,23 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s3;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-while (s1 && s1[len1])
-len1++;
-while (s2 && s2[len2])
-len2++;
+unsigned int i = 0, j = 0, len1 = strlen(s1), len2 = strlen(s2);
 s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 if (s3 == NULL)
 return (NULL);
-i = 0;
-j = 0;
-if (s1)
+if (s1 && s2)
 {
-while (i < len1)
+for(i = 0, j = 0; i < (len1 + len2); i++)
+{
+if (i < len1)
 {
 s3[i] = s1[i];
-i++;
 }
-}
-if (s2)
-{
-while (i < (len1 + len2))
+else
 {
 s3[i] = s2[j];
-i++;
 j++;
+}
 }
 }
 s3[i] = '\0';
