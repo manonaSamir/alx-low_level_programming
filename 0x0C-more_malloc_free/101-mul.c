ffe9f063a854201592bfa1397250ce7e2687aedc
manonaSamir <bena.yalla@yahoo.com>
2023-10-07 23:23:56 +0300

    more malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,30 @@
 #include <string.h>
 #include <ctype.h>
 
+/**
+ * checkErrors - check Errors
+ */
+int checkErrors(int c, char *v[])
+{
+int j, i;
+if (c != 3)
+{
+printf("Error\n");
+return (98);
+}
+for (i = 1; i < c; i++)
+{
+for (j = 0; v[i][j] != '\0' ; j++)
+{
+if (!isdigit(v[i][j]))
+{
+printf("Error\n");
+return (98);
+}
+}
+}
+return (0);
+}
 
 /**
  * main - multiplies two positive numbers
@@ -14,24 +38,9 @@
 
 int main(int argc, char *argv[])
 {
-int len, len1, len2, *result_str;
-int j, i, digit1, digit2, sum;
-if (argc != 3)
-{
-printf("Error\n");
-return (98);
-}
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0' ; j++)
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (98);
-}
-}
-}
+int i, len, len1, len2, *result_str;
+int digit1, digit2, sum;
+checkErrors(argc, argv);
 len1 = strlen(argv[1]);
 len2 = strlen(argv[2]);
 len = len1 + len2 + 1;
@@ -47,7 +56,7 @@ sum = 0;
 for (len2 = strlen(argv[2]) - 1; len2 >= 0; len2--)
 {
 digit2 = argv[2][len2] - '0';
-sum += result_str[len1 + len2 + 1] + (digit1 * digit2);
+sum += result_str[len] + (digit1 * digit2);
 result_str[len1 + len2 + 1] = sum % 10;
 sum /= 10;
 }
