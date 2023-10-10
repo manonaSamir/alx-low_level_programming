246511b2428305b3c09fc442a6b9a84b433965a9
manonaSamir <bena.yalla@yahoo.com>
2023-10-10 20:46:00 +0300

    structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,10 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return;
 if ((*d).name == NULL)
-(*d).name = "Name: (nil)";
+(*d).name = "(nil)";
 if ((*d).owner == NULL)
-(*d).owner = "Owner: (nil)";
+(*d).owner = "(nil)";
 if ((*d).age == 0)
-(*d).owner = "age: (nil)";
+(*d).owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
