e366f301ff348cf71da287081678c3c583f2fd10
manonaSamir <bena.yalla@yahoo.com>
2023-10-11 21:02:00 +0300

    function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ if (!ptr2func)
 printf("Error\n");
 exit(99);
 }
-if (( *argv[2] == '/' ||  *argv[2] == '%') && b == 0)
+if ((*argv[2] == '/' ||  *argv[2] == '%') && b == 0)
 {
 printf("Error\n");
 exit(100);
