9254ca13c75944e2f061a1f5493abe44e249ef1b
manonaSamir <bena.yalla@yahoo.com>
2023-10-23 22:42:52 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,16 +12,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	if (*head)
-	{
-		while ((*head))
-		{
-			current = (*head)->next;
-			free(*head);
-			*head = current;
-		}
-		*head = NULL;
-	}
-	else
+	if (head == NULL)
 		return;
+	while ((*head))
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+	*head = NULL;
 }
