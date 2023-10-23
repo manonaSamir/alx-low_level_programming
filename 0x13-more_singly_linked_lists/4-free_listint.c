c4c96ed4fa3c18c947854a9027c3b68340decbe5
manonaSamir <bena.yalla@yahoo.com>
2023-10-23 22:01:35 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,8 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *current;
-
-	while (head)
-	{
-		current = head->next;
-		free(head);
-		head = current;
-	}
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
 }
