055b572a44e1c288c746fa40aa29fac41812a73a
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 11:19:40 +0300

    Signly linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer to list.
+ * @index: is the index of the node, starting at 0
+ * Return: number of elements in a list
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *current = head;
+
+	if (current)
+	{
+		for (i = 0; i < index; i++)
+		{
+			current = current->next;
+		}
+		return (current);
+	}
+	return (NULL);
+}
