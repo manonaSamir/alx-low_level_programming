ae893db42308afb222b7769f1cb511661300a4c1
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 15:05:06 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	else
 	{
 		if (!previous || !current)
-			return (0);
+			return (-1);
 		while (index != 1)
 		{
 			previous = current;
