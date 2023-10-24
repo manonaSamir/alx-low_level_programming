1d26914c88a6901e3f8e2629e4ea3ba9c286f2e6
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 11:59:33 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head || !current)
 		return (NULL);
 
-	for (; i < index; i++)
+	for (; i < index && current; i++)
 	{
 		current = current->next;
 	}
