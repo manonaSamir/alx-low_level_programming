fa3dbb03dc8e6874572cb42d9747ce1c0781b78b
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 13:27:22 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2->n = n;
 	ptr2->next = NULL;
 	idx--;
-	while (idx != 1)
+	while (idx != 1 && ptr2)
 	{
 		ptr = ptr->next;
 		idx--;
