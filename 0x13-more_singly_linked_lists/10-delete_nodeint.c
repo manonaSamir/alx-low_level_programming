c1dfbf52e586bcb2968d37098da718a3797a7689
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 14:44:27 +0300

    Signly linked list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a linked list.
+ * @head: pointer to list.
+ * @index: index where the new node is added
+ * Return: number of elements in a list
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *previous = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
+	else if (index == 1)
+	{
+		*head = current->next;
+		free(current);
+		current = NULL;
+	}
+	else
+	{
+		if (!previous || !current)
+			return (0);
+		while (index != 1)
+		{
+			previous = current;
+			current = current->next;
+			index--;
+		}
+		previous->next = current->next;
+		free(current);
+		current = NULL;
+	}
+	return (1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr = ptr->next;
 		idx--;
 	}
-
 	ptr2->next = ptr->next;
 	ptr->next = ptr2;
 	return (ptr2);
