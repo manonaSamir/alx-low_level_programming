3235fe545e10ec81f14faf0855abaa1a217844de
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 11:37:47 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current;
 
-	if (!head)
+	if (!head || index == 0)
 		return (NULL);
 	current = head;
 	for (i = 0; i < index; i++)
