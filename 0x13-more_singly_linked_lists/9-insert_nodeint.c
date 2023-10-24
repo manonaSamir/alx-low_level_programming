18cb46acc1817db17f34e3418f768389f72b361a
manonaSamir <bena.yalla@yahoo.com>
2023-10-24 13:32:44 +0300

    Signly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	ptr2->next = ptr->next;
 	ptr->next = ptr2;
-	if(ptr2)
+	if (ptr2)
 		return (ptr2);
 	return (NULL);
 }
