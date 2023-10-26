e297af0a88ff8ae6026fbfb5c0e52c2975409672
manonaSamir <bena.yalla@yahoo.com>
2023-10-26 15:12:50 +0300

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, b_num;
+	unsigned int i, b_num = 0;
 
 	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != 0 || b[i] != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (b[i] == 1)
+		if (b[i] == '1')
 			b_num += 1;
 		b_num <<= 1;
 	}
