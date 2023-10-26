8d5f94ef97cfc79e3fc41c02ca7c51c1594e4fd4
manonaSamir <bena.yalla@yahoo.com>
2023-10-26 23:16:36 +0300

    bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,24 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, size = sizeof(n) * 8;
-
-	if (n == 0)
+	if (n > 1)
 	{
-		_putchar('0');
-		return;
+		print_binary(n >> 1);
 	}
-
-	for (i = 0; i < size; i++)
-	{
-		if (n == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
-		n <<= 1;
-	}
+	_putchar((n & 1) + '0');
 }
