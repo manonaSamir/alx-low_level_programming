e00111026249d94d32744053d7eefa407a26a0ce
manonaSamir <bena.yalla@yahoo.com>
2023-10-26 23:43:40 +0200

    bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,10 +2,7 @@
 #include <stdio.h>
 
 /**
- * flip_bits - function that returns the number of bits you would need to flip
- * to get from one number to another.
- * @n: starting
- * @m: target
+ * get_endianness - checks the endianness.
  *
  * Return: number bits flipped
  */
