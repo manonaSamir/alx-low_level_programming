5dc3abf4e3f910a7cc6d889541be7f4e4c38deae
manonaSamir <bena.yalla@yahoo.com>
2023-10-31 00:10:51 +0200

    File i/o

---
+++ 0x15-file_io/strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to measure.
+ *
+ * Return: Length of the string as INT.
+ */
+
+int _strlen(char *s)
+{
+	if (*s)
+		return (1 + _strlen((s + 1)));
+	return (0);
+}
