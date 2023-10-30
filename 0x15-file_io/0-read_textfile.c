d6a492692c7c06173cfb562b324fc95ea536d340
manonaSamir <bena.yalla@yahoo.com>
2023-10-30 17:02:08 +0200

    File i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ * function should read and print.
+ * Return: If the function fails or filename is NULL - 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	size_t i = 0;
+	ssize_t count = 0;
+	FILE *fptr;
+	char ch;
+
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+	{
+		return (0);
+	}
+
+	for (; i < letters; i++)
+	{
+		ch = fgetc(fptr);
+		if (feof(fptr))
+		{
+			break;
+		}
+		printf("%c", ch);
+		count++;
+	}
+
+	fclose(fptr);
+	return (1);
+}
