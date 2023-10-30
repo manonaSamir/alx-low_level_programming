f54ca5500e1f26b1c3de47161bbfc5e44250e3bb
manonaSamir <bena.yalla@yahoo.com>
2023-10-30 22:24:45 +0200

    File i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ * Return: If fails -(-1)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int count, length;
+
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+	}
+	if (filename == NULL)
+		return (-1);
+	count = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	count = write(count, text_content, length);
+	if (count == -1)
+		return (-1);
+	close(count);
+	return (1);
+}
