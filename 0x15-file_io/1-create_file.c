110867d6abcae0d3ae9a85f9f4b2fe759af6b191
manonaSamir <bena.yalla@yahoo.com>
2023-10-30 23:09:43 +0200

    File i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	fptr = open(filename, O_CREAT , S_IRUSR | S_IWUSR);
 	if (fptr == -1)
 		return (-1);
 	if (!length)
