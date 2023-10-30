df08129490c38f274f8ae33d0b191c7c0eccd5e9
manonaSamir <bena.yalla@yahoo.com>
2023-10-30 23:33:16 +0200

    File i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,10 +27,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-		length = _strlen(text_content);
 
-	fptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC,  S_IWUSR);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	count = write(fptr, text_content, length);
 	if (fptr == -1 || count == -1)
 		return (-1);
