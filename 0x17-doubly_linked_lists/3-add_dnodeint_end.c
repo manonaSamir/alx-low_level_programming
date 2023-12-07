34fdaa7a230e62dc65732427e1b3654606ed1b8f
manonaSamir <bena.yalla@yahoo.com>
2023-12-07 21:08:48 +0200

    check nullable

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,6 +12,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	tube = *head;
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 	new->prev = NULL;
 	new->n = n;
 	new->next = NULL;
