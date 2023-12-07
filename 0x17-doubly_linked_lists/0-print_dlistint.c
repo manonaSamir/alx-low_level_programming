e14d919291b2e0a94aae4ecb2f5a0dcc18dfb9dc
manonaSamir <bena.yalla@yahoo.com>
2023-12-07 19:01:07 +0200

    configure

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,19 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t add_nodes;
+	size_t add_nodes = 0;
 
 	if (!h)
 		return (0);
-	add_nodes = 0;
-	do {
-		if (!h->n)
-			printf("[0] (nil)\n");
-		else
-			printf("%d \n", h->n);
-
+	while (h)
+	{
+		printf("%d \n", h->n);
 		h = h->next;
 		add_nodes++;
-	} while (h);
+	}
 	return (add_nodes);
 }
