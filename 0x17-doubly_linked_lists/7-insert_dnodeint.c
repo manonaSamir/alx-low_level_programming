dcebfd9712eadd6f496ec89459a1dabcff44c0b6
manonaSamir <bena.yalla@yahoo.com>
2023-12-07 23:50:28 +0200

    ggg

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_dnodeint_at_index - insert node at index
  * @h: pointer of  list
@@ -12,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int siz = 1;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL || h == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	/* to get the size of the list */
@@ -46,5 +47,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	fast->prev = new_node;
 	slow->next = new_node;
 
-	return new_node;
+	return (new_node);
 }
