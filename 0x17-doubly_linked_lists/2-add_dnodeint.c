2462120670c1b611285d7d132ed7a9fbe7658b03
manonaSamir <bena.yalla@yahoo.com>
2023-12-07 21:08:03 +0200

    checknullable

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 	new->prev = NULL;
 	new->n = n;
 	new->next = NULL;
