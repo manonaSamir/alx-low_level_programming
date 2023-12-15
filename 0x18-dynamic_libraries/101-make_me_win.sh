8efddfa2e0a33d9c6ca9c37abf9755d6399f7886
manonaSamir <bena.yalla@yahoo.com>
2023-12-15 23:48:22 +0200

    changes last task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p https://github.com/manonaSamir/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD ~/tmp/libgiga.so
+wget -p /tmp https://github.com/manonaSamir/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD~/tmp/libgiga.so
